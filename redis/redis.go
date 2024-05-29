54baba6e4b8c78aa62e75f6046e4440539e3e12e
Ju-Wiluis William Rodriguez Hernandez <jwrodriguezh.externo@bcds.net>
2024-05-29 22:53:24 +0200

    Added Singleton RedisClient

--- redis/redis.go
+++ redis/redis.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/Luis97lol/auth-service/auth"
@@ -11,18 +12,22 @@ import (
 )
 
 var redisClient *redis.Client
+var once sync.Once
 
-func init() {
-	redisClient = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
+func GetInstance() *redis.Client {
+	once.Do(func() {
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     os.Getenv("REDIS_ADDR"),     // Dirección del servidor Redis
+			Password: os.Getenv("REDIS_PASSWORD"), // Contraseña (si es necesario)
+			DB:       0,                           // Número de base de datos
+		})
 	})
+	return redisClient
 }
 
 func GenerateToken(userId string) (string, error) {
 	token := fmt.Sprintf("token:%s", auth.GenerateJWT(auth.User{Id: userId}))
-	err := redisClient.Set(context.Background(), token, userId, 5*time.Minute).Err()
+	err := GetInstance().Set(context.Background(), token, userId, 5*time.Minute).Err()
 	if err != nil {
 		return "", err
 	}
@@ -30,7 +35,7 @@ func GenerateToken(userId string) (string, error) {
 }
 
 func ValidateToken(token string) (string, error) {
-	val, err := redisClient.Get(context.Background(), token).Result()
+	val, err := GetInstance().Get(context.Background(), token).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("token not found")
 	} else if err != nil {
