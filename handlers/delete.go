859e6e1e0071ec0496592d858c77be1372fd6d66
Ju-Wiluis William Rodriguez Hernandez <jwrodriguezh.externo@bcds.net>
2024-06-23 23:20:56 +0200

    Fixed bug with DeleteHandler

--- handlers/delete.go
+++ handlers/delete.go
@@ -5,15 +5,22 @@ import (
 	"net/http"
 
 	"github.com/Luis97lol/auth-service/database"
+	"github.com/Luis97lol/auth-service/redis"
 	"github.com/gorilla/mux"
 )
 
 func DeleteHandler(w http.ResponseWriter, r *http.Request) {
-	// Read token from Authorization header
-	LogoutHandler(w, r)
 	vars := mux.Vars(r)
 	oid := vars["oid"]
 	userId := vars["userId"]
+
+	token := extractToken(r.Header.Get("Authorization"))
+	// Validate token against Redis
+	_, err := redis.ValidateToken(token)
+	if err == nil {
+		redis.DeleteToken(token, userId)
+	}
+
 	database.DeleteUser(oid, userId)
 
 	// Respond with username
